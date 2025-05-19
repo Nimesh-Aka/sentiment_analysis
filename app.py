a0211e11de61c67bed2d0f4a6bc925b453af6615
Nimesh Akarshana <masteraka524@gmail.com>
2025-05-19 19:39:20 +0530

    Add logging functionality to track application events and predictions

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect
 from helper import vectorizer, get_prediction, preprocessing
+from logger import logging
 
 app = Flask(__name__)
+
+logging.info("Flask server started")
 
 data = dict()
 reviews = []
@@ -13,14 +16,24 @@ def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+
+    logging.info("=========== Open Home Page ==============" )
     return render_template('index.html', data=data)
+
+
 
 @app.route('/', methods=['POST'])
 def my_post():
+    logging.info("=========== Open Post Page ==============" )
     text = request.form['text']
-    preprossed_text = preprocessing(text)
-    vectorized_text = vectorizer(preprossed_text)
+
+    logging.info("Text: %s", text)
+    preprocessed_text = preprocessing(text)
+    logging.info("Preprocessed Text: %s", preprocessed_text)
+    vectorized_text = vectorizer(preprocessed_text)
+    logging.info("Vectorized Text: %s", vectorized_text)
     prediction = get_prediction(vectorized_text)
+    logging.info("Prediction: %s", prediction)
 
     if prediction == "Positive":
         global positive
